af53b3aee2dd3c0d0a7680f2bd044cdfcac37631
Samuel Setarige <setarigesamuel@gmail.com>
2025-07-23 15:53:13 +0300

    Fix register page to use correct API base URL

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/', (req, res) => {
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
